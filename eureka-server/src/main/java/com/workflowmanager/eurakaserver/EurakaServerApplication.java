9d97bc7ece39dcf037b5563e2308e56469441757
yibingq946 <53620290+yibingq946@users.noreply.github.com>
2019-08-18 22:02:33 -0400

    create gateway and eureka-server

---
+++ eureka-server/src/main/java/com/workflowmanager/eurakaserver/EurakaServerApplication.java
@@ -0,0 +1,17 @@
+package com.workflowmanager.eurakaserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
+public class EurakaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurakaServerApplication.class, args);
+    }
+
+}
